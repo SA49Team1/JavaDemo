03a89d307676b27a087e9d208def63b84d4de9aa
Shawn Wang <Shawn Wang@DESKTOP-MBSGJ0P>
2019-11-30 20:58:32 +0800

    first commit

---
+++ Demo/src/main/java/SA49Team1/Demo/App.java
@@ -0,0 +1,13 @@
+package SA49Team1.Demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
